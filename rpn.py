ae8ce00c23c41ff21c53c0a9b5dda7a82e3a8394
jakepete <jakepete@umich.edu>
2017-10-27 13:18:38 -0400

    init commit

---
+++ rpn.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import operator
+
+
+opers = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+
+}
+
+
+def calculate(arg):
+    stack = list()
+    for token in arg.split():
+        try:
+            stack.appent(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            calc = opers[token]
+            stack.append(calc(arg1, arg2))
+
+    return stack.pop()
+
+
+def main()
+    while True:
+        calculate(input("rpn calc> "))
+
+
+if __name__ == '__main__':
+    main()
